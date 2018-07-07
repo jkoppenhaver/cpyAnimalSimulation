a4bb36691efa3b660f78e131dbd9506120cd521b
* <jkoppp267@gmail.com>
2018-07-07 17:21:14 -0500

    Initial Commit

---
+++ animals.py
@@ -0,0 +1,198 @@
+import random
+
+###############################################################################
+# Class Animal
+# Descrption: This class describes the basic animal attributes and methods
+###############################################################################
+class Animal(object):
+###############################################################################
+# __init__
+# Descrption: This function initializes a new animal object.
+# Arguments: this_color - The color this animal should be represented as
+#            blank_color - The backround color that signifies a space is unoccupied
+#            dispBuff - The display buffer this object should use
+# Returns: Nothing
+###############################################################################
+	def __init__(self, this_color, blank_color, dispBuff):
+		self.blank_color = blank_color
+		self.color = this_color
+		self.x = random.randint(0,len(dispBuff)-1)
+		self.y = random.randint(0,len(dispBuff[0])-1)
+		while(dispBuff[self.x][self.y] != self.blank_color):
+			self.x = random.randint(0,len(disp)-1)
+			self.y = random.randint(0,len(disp[0])-1)
+		dispBuff[self.x][self.y] = self.color
+		return
+###############################################################################
+# __decideDirection
+# Descrption: This function decides how the animal should move next.
+#             This function is private and should only be called from within the class
+# Arguments: dispBuff - The display buffer this object should use
+# Returns: The chosen direction.  This direction is guarenteed to be a valid move
+###############################################################################
+	def __decideDirection(self, dispBuff):
+		direction = random.randint(0,3)
+		while(not self.__isValidMove(direction, dispBuff)):
+			direction = random.randint(0,3)
+		return direction
+	
+###############################################################################
+# __isValidMove
+# Descrption: This function determines if a direction is safe to move in.
+#             The default animal can move into any space that is on the display and unoccupied
+# Arguments: direction - The direction to be checked
+#            dispBuff - The display buffer this object should use
+# Returns: Boolean representing if the move is valid or not
+###############################################################################
+	def __isValidMove(self, direction, dispBuff):
+		valid = False
+		if direction == 0:
+			#Move down
+			if((self.y+1) < len(dispBuff[0]) and dispBuff[self.x][self.y+1] == self.blank_color):
+				valid = True
+		elif direction == 1:
+			#Move right
+			if((self.x+1) < len(dispBuff) and dispBuff[self.x+1][self.y] == self.blank_color):
+				valid = True
+		elif direction == 2:
+			#Move up
+			if((self.y-1) >= 0 and dispBuff[self.x][self.y-1] == self.blank_color):
+				valid = True
+		elif direction == 3:
+			#Move left
+			if((self.x-1) >= 0 and dispBuff[self.x-1][self.y] == self.blank_color):
+				valid = True
+		return valid
+	
+###############################################################################
+# move
+# Descrption: This function moves the animal one space in the direction decided by __decideDirection
+#             It moves the animal and then updates the display buffer to render the new location.
+#             *Note* This function only updates the display buffer.  It will not change the actual display.
+# Arguments: dispBuff - The display buffer this object should use
+# Returns: Boolean representing if the move is valid or not
+###############################################################################
+	def move(self, dispBuff):
+		direction = self.__decideDirection(dispBuff)
+		dispBuff[self.x][self.y] = self.blank_color
+		if direction == 0:
+			self.y += 1
+		elif direction == 1:
+			self.x += 1
+		elif direction == 2:
+			self.y -= 1
+		elif direction == 3:
+			self.x -= 1
+		dispBuff[self.x][self.y] = self.color
+		return
+
+###############################################################################
+# Class Prey
+# Parent: Animal
+# Descrption: This class inherits from Animal and overloads the some of
+#             the functions to provide prey specific behavior.  The only
+#             difference from Animal is that prey have 4 'directions' they
+#             can choose from.  They can move up, down, left , right, or hide.
+#             When prey hide they do not move at all and continue to occupy
+#             their current space.
+###############################################################################
+class Prey(Animal):
+	def __decideDirection(self, dispBuff):
+		direction = random.randint(0,4)
+		while((not self.__isValidMove(direction, dispBuff)) and direction != 4):
+			direction = random.randint(0,4)
+		return direction
+	def move(self, dispBuff):
+		direction = self.__decideDirection(dispBuff)
+		if direction != 4:
+			dispBuff[self.x][self.y] = self.blank_color
+			if direction == 0:
+				self.y += 1
+			elif direction == 1:
+				self.x += 1
+			elif direction == 2:
+				self.y -= 1
+			elif direction == 3:
+				self.x -= 1
+			dispBuff[self.x][self.y] = self.color
+		return
+
+###############################################################################
+# Class Predator
+# Parent: Animal
+# Descrption: This class inherits from Animal and overloads the some of
+#             the functions to provide predator specific behavior.  This
+#             includes chasing prey instead of moving randomly.
+###############################################################################
+class Predator(Animal):
+###############################################################################
+# __init__
+# Descrption: This function initializes a new predator object.  It is
+#             the same as the Animal initializer except that it takes an
+#             extra argument that represents what color it should search
+#             for and chase.
+# Arguments: this_color - The color this animal should be represented as
+#            blank_color - The backround color that signifies a space is unoccupied
+#            prey_color - The prey color that this predator should chase
+#            dispBuff - The display buffer this object should use
+# Returns: Nothing
+###############################################################################
+	def __init__(self, this_color, blank_color, prey_color, dispBuff):
+		self.blank_color = blank_color
+		self.color = this_color
+		self.prey = prey_color
+		self.x = random.randint(0,len(dispBuff)-1)
+		self.y = random.randint(0,len(dispBuff[0])-1)
+		while(dispBuff[self.x][self.y] != self.blank_color):
+			self.x = random.randint(0,len(disp)-1)
+			self.y = random.randint(0,len(disp[0])-1)
+		dispBuff[self.x][self.y] = self.color
+		return
+	
+###############################################################################
+# __decideDirection
+# Descrption: This function decides how the animal should move next.
+#             This function is private and should only be called from within the class
+#             For the predator, it searches for prey and moves towards the closest one.
+#             It also differs from the default __decideDirection method in that it does
+#             not matter if the space is occupied.
+# Arguments: dispBuff - The display buffer this object should use
+# Returns: The chosen direction.  This direction is guarenteed to be a valid move
+###############################################################################
+	def __decideDirection(self, dispBuff):
+		direction = 0
+		prey_locations = []
+		# Look for prey and add all found locations to the prey_locations list
+		for i in range(len(dispBuff)):
+			for j in range(len(dispBuff[i])):
+				if(dispBuff[i][j] == self.prey):
+					prey_locations.append((i,j))
+		if len(prey_locations) == 0:
+			#No prey found so just move randomly
+			direction = random.randint(0,3)
+			while(not self.__isValidMove(direction, dispBuff)):
+				direction = random.randint(0,3)
+		else:
+			# Determine the closest prey
+			smallest_diff = 20
+			smallest_index = 0
+			for i in range(len(prey_locations)):
+				diff = abs(prey_locations[i][0] - self.x)+abs(prey_locations[i][1] - self.y)
+				if diff < smallest_diff:
+					smallest_diff = diff
+					smallest_index = i
+			xdiff = prey_locations[smallest_index][0] - self.x
+			ydiff = prey_locations[smallest_index][1] - self.y
+			# Because the predators can not move diagonally, pick the axis with the largest
+			# difference and move in that direction towards the prey.
+			if(abs(xdiff) > abs(ydiff)):
+				if(xdiff > 0):
+					direction = 1
+				else:
+					direction = 3
+			else:
+				if(ydiff > 0):
+					direction = 0
+				else:
+					direction = 2
+		return direction
---
+++ main.py
@@ -0,0 +1,72 @@
+import neopixel
+import board
+import time
+import Animals
+
+WOLF_COLOR = (255,0,0)
+RABBIT_COLOR = (40,40,20)
+BLANK_COLOR = (0,10,0)
+
+# Initialize and turn off the on board RGB LED
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
+dot[0] = (0,0,0)
+
+# Initilize the NeoPixel FeatherWing
+# It has 32 LEDs and is by default connected to Pin D6
+disp = neopixel.NeoPixel(board.D6, 32, brightness=0.2, auto_write=False)
+
+# Create a display buffer to hold the pixel data
+# This is a 2D array that more accurately represents the pixel layout
+#  then the long 1D array of NeoPixles
+# The FeatherWing is 4 LEDs tall and 8 wide
+# Fill the buffer with the blank color
+h = 4
+w = 8
+pdata = [[BLANK_COLOR for i in range(h)] for j in range(w)]
+
+###############################################################################
+# updateDisplay()
+# Descrption: This function converts the 2D display buffer to the 1D array the
+#             NeoPixels use and writes the values to the display.
+# Arguments: None
+# Returns: Nothing
+###############################################################################
+def updateDisplay():
+	global pdata
+	for i in range(len(pdata)):
+		for j in range(len(pdata[i])):
+			disp[(j*8)+i] = pdata[i][j]
+	disp.show()
+	return
+	
+#Clear the Display
+updateDisplay()
+#Generate one wolf
+wolf = Animals.Predator(WOLF_COLOR, BLANK_COLOR, RABBIT_COLOR, pdata)
+#Generate a list to hold all of the rabbits and add a bunch of them
+rabbits = []
+rabbits.append(Animals.Prey(RABBIT_COLOR, BLANK_COLOR, pdata))
+rabbits.append(Animals.Prey(RABBIT_COLOR, BLANK_COLOR, pdata))
+rabbits.append(Animals.Prey(RABBIT_COLOR, BLANK_COLOR, pdata))
+rabbits.append(Animals.Prey(RABBIT_COLOR, BLANK_COLOR, pdata))
+rabbits.append(Animals.Prey(RABBIT_COLOR, BLANK_COLOR, pdata))
+rabbits.append(Animals.Prey(RABBIT_COLOR, BLANK_COLOR, pdata))
+# Update the display to show all of the newly created animals
+updateDisplay()
+while(True):
+	# Check to see if any rabbits have been eaten and remove them if they have been
+	for i in range(len(rabbits)):
+		if(wolf.x == rabbits[i].x) and (wolf.y == rabbits[i].y):
+			rabbits.pop(i)
+			break
+	# Move all the remaining rabbits
+	for rabbit in rabbits:
+		rabbit.move(pdata)
+	# Update the display to show the rabbits movement and wait for a little before moving the wolves
+	updateDisplay()
+	time.sleep(0.5)
+	# Move the wolf
+	wolf.move(pdata)
+	# Update the display to show the wolf's movement and wait for a little before moving the rabbits again
+	updateDisplay()
+	time.sleep(0.3)
