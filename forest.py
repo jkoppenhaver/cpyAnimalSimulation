358a5e7ae270c6613b9d553e5d032157f18418b2
akjroller <akjroller@gmail.com>
2021-05-31 01:51:57 -0400

    Fixed spelling mistakes
    Fixed spelling mistakes and python best practices

--- forest.py
+++ forest.py
@@ -3,16 +3,16 @@ import board
 import time
 import Animals
 
-WOLF_COLOR = (255,0,0)
-BEAR_COLOR = (127,127,0)
-RABBIT_COLOR = (40,40,20)
-BLANK_COLOR = (0,10,0)
+WOLF_COLOR = (255, 0, 0)
+BEAR_COLOR = (127, 127, 0)
+RABBIT_COLOR = (40, 40, 20)
+BLANK_COLOR = (0, 10, 0)
 
 # Initialize and turn off the on board RGB LED
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
-dot[0] = (0,0,0)
+dot[0] = (0, 0, 0)
 
-# Initilize the NeoPixel FeatherWing
+# Initialize the NeoPixel FeatherWing
 # It has 32 LEDs and is by default connected to Pin D6
 disp = neopixel.NeoPixel(board.D6, 32, brightness=0.2, auto_write=False)
 
@@ -25,51 +25,49 @@ h = 4
 w = 8
 pdata = [[BLANK_COLOR for i in range(h)] for j in range(w)]
 
+
 ###############################################################################
 # updateDisplay()
-# Descrption: This function converts the 2D display buffer to the 1D array the
+# Description: This function converts the 2D display buffer to the 1D array the
 #             NeoPixels use and writes the values to the display.
 # Arguments: None
 # Returns: Nothing
 ###############################################################################
 def updateDisplay():
-	global pdata
-	for i in range(len(pdata)):
-		for j in range(len(pdata[i])):
-			disp[(j*8)+i] = pdata[i][j]
-	disp.show()
-	return
-	
-#Clear the Display
+    global pdata
+    for i in range(len(pdata)):
+        for j in range(len(pdata[i])):
+            disp[(j * 8) + i] = pdata[i][j]
+    disp.show()
+    return
+
+
+# Clear the Display
 updateDisplay()
-#Generate one wolf and one bear
-predators = []
-predators.append(Animals.Predator(WOLF_COLOR, BLANK_COLOR, RABBIT_COLOR, pdata))
-predators.append(Animals.Predator(BEAR_COLOR, BLANK_COLOR, RABBIT_COLOR, pdata))
-#Generate a list to hold all of the rabbits and add a bunch of them
-prey = []
-prey.append(Animals.Prey(RABBIT_COLOR, BLANK_COLOR, pdata))
-prey.append(Animals.Prey(RABBIT_COLOR, BLANK_COLOR, pdata))
-prey.append(Animals.Prey(RABBIT_COLOR, BLANK_COLOR, pdata))
-prey.append(Animals.Prey(RABBIT_COLOR, BLANK_COLOR, pdata))
+# Generate one wolf and one bear
+predators = [Animals.Predator(WOLF_COLOR, BLANK_COLOR, RABBIT_COLOR, pdata),
+             Animals.Predator(BEAR_COLOR, BLANK_COLOR, RABBIT_COLOR, pdata)]
+# Generate a list to hold all of the rabbits and add a bunch of them
+prey = [Animals.Prey(RABBIT_COLOR, BLANK_COLOR, pdata), Animals.Prey(RABBIT_COLOR, BLANK_COLOR, pdata),
+        Animals.Prey(RABBIT_COLOR, BLANK_COLOR, pdata), Animals.Prey(RABBIT_COLOR, BLANK_COLOR, pdata)]
 # Update the display to show all of the newly created animals
 updateDisplay()
-while(True):
-	# Move all the remaining rabbits
-	for p in prey:
-		p.move(pdata)
-	# Update the display to show the rabbits movement and wait for a little before moving the wolves
-	updateDisplay()
-	time.sleep(0.5)
-	# Move the predators
-	for predator in predators:
-		predator.move(pdata)
-		# Check to see if any prey were caught and remove them if they have been
-		for i in range(len(prey)):
-			if(predator.prey == prey[i].color):
-				if(predator.x == prey[i].x) and (predator.y == prey[i].y):
-					prey.pop(i)
-					break
-	# Update the display to show the movement and wait for a little before moving the prey again
-	updateDisplay()
-	time.sleep(0.3)
+while True:
+    # Move all the remaining rabbits
+    for p in prey:
+        p.move(pdata)
+    # Update the display to show the rabbits movement and wait for a little before moving the wolves
+    updateDisplay()
+    time.sleep(0.5)
+    # Move the predators
+    for predator in predators:
+        predator.move(pdata)
+        # Check to see if any prey were caught and remove them if they have been
+        for i in range(len(prey)):
+            if predator.prey == prey[i].color:
+                if (predator.x == prey[i].x) and (predator.y == prey[i].y):
+                    prey.pop(i)
+                    break
+    # Update the display to show the movement and wait for a little before moving the prey again
+    updateDisplay()
+    time.sleep(0.3)
